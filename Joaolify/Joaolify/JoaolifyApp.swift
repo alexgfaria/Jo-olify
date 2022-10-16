3ccf4d974fe5c8c95ab4f50db4299e8fa4ea24ac
Alex G. Faria <alex.faria@farfetch.com>
2022-10-16 18:26:27 +0100

    Creates project

---
+++ Joaolify/Joaolify/JoaolifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  JoaolifyApp.swift
+//  Joaolify
+//
+//  Created by Alex Faria on 16/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct JoaolifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
